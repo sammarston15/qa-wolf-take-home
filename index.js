a46acfd91739f7aa5c5d5505fa2c91c2fafb6490
Sam Marston <sammarston15@gmail.com>
2025-03-09 11:05:36 -0700

    add: loop through ranks

--- index.js
+++ index.js
@@ -9,8 +9,26 @@ async function sortHackerNewsArticles() {
 
   // go to Hacker News
   await page.goto("https://news.ycombinator.com/newest");
+
+  // get the rank and time posted from each news article
+  
+  let nth = 0
+
+
+
+  // log all ranks on the page
+  for (const li of await page.locator('.rank').all()) {
+    console.log(await li.innerHTML())
+    // await ranks.push(li.innerHTML())
+  }
+
+
+
+
+  // return await ranks
+  
 }
 
 (async () => {
-  await sortHackerNewsArticles();
+  console.log(await sortHackerNewsArticles());
 })();
